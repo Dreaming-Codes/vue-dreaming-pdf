91d95d3e03b131661bdfd7b6bd671f17ab4db404
DreamingCodes <dev@dreaming.codes>
2022-02-26 21:37:33 +0100

    Finished renderer

---
+++ src/components/FormEditor.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "FormEditor"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export {default as Viewer} from './Viewer.vue';
+export {default as FormEditor} from "./FormEditor.vue";
